f16babb6dd980bddf664cbe47cbfdf800d9eb9fb
VovaSles <vovaswebdev@gmail.com>
2023-11-17 15:33:55 +0200

    auth pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import "./globals.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import Home from "./_root/pages/Home";
+import AuthLayout from "./_auth/forms/AuthLayout";
+import RootLayout from "./_root/pages/RootLayout";
+import SignupForm from "./_auth/forms/SignupForm";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <main className="flex h-screen">
+      <Routes>
+        {/*publlic*/}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-up" element={<SignupForm/>} />
+          <Route path="/sign-in" element={<SigninForm />} />
+        </Route>
+        {/*private*/}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
 
-export default App
+export default App;
---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+
+const SignupForm = () => {
+  return (
+    <Button variant="outline">Button</Button>
+
+  )
+}
+
+export default SignupForm
